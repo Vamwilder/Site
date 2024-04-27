07264e40bd9dc1d3d55ac4ae0eaf6945497fd9ea
Maik <kiamdemouralechinovski@hotmail.com>
2024-04-27 10:20:23 -0300

    Adicionar arquivos de projeto.

---
+++ LoginSenhaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginSenhaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LoginSenhaWeb/Views/Home/Contatos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginSenhaWeb.Views.Home
+{
+    public class ContatosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
