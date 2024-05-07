559df89176c83b5e61929231e6f7abf92890da08
Maik <kiamdemouralechinovski@hotmail.com>
2024-05-07 16:35:55 -0300

    Tela de registro de novos Usuários

--- LoginSenhaWeb/Controllers/AccountController.cs
+++ LoginSenhaWeb/Controllers/AccountController.cs
@@ -4,22 +4,38 @@ using LoginSenhaWeb.Models;
 namespace LoginSenhaWeb.Controllers
 {
     public class AccountController : Controller
-    {       
-        
+    {               
 
         public IActionResult Logado(string username, string password)
         {
             if(DataBase.UserExists(username, password) == true)
             {
+                TempData["ErrorMessage"] = null;
                 return View();
             }else
             {
+                TempData["ErrorMessage"] = "Usuário ou senha incorretos.";
                 return RedirectToAction("Login", "Home");
             }
         }
-        public IActionResult Register()
+        public IActionResult Register(string username, string password)
         {
-            return RedirectToAction("Index", "Home");
+            TempData["ErrorMessage"] = null;
+            TempData["SucessMessage"] = null;
+            if (username == null || password == null)
+            {
+                TempData["ErrorMessage"] = "Por favor digite usuário e senha";
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                TempData["SucessMessage"] = "Usuário cadastrado com sucesso!";
+                return RedirectToAction("Login", "Home");
+            }            
+        }
+        public IActionResult NovoUsuario()
+        {
+            return View();
         }
     }
 }
--- LoginSenhaWeb/Models/DataBase.cs
+++ LoginSenhaWeb/Models/DataBase.cs
@@ -47,5 +47,7 @@ namespace LoginSenhaWeb.Models
             }
         }
 
+
+
     }
 }
